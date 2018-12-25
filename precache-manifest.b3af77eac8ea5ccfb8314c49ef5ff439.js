ee3c0f393d906bcffea2e8066885c13c45bdaee1
Виталий Набережный <naberezhniy@taxi21.ru>
2018-12-25 16:02:27 +0300

    Updates

---
+++ precache-manifest.b3af77eac8ea5ccfb8314c49ef5ff439.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f46862784c1be7da940702ff568a299a",
+    "url": "/zacistijkrai/static/media/logo.f4686278.png"
+  },
+  {
+    "revision": "25e71cdc6d93397cbd550a519f115e43",
+    "url": "/zacistijkrai/static/media/bg2.25e71cdc.jpg"
+  },
+  {
+    "revision": "a0b99120d271fee685ba240717215abd",
+    "url": "/zacistijkrai/static/media/bg1.a0b99120.jpg"
+  },
+  {
+    "revision": "d319fae740a9933acb6d",
+    "url": "/zacistijkrai/static/js/runtime~main.d319fae7.js"
+  },
+  {
+    "revision": "873142b3821d09db7a17",
+    "url": "/zacistijkrai/static/js/main.873142b3.chunk.js"
+  },
+  {
+    "revision": "0d2494b12d08a33ca4c0",
+    "url": "/zacistijkrai/static/js/1.0d2494b1.chunk.js"
+  },
+  {
+    "revision": "873142b3821d09db7a17",
+    "url": "/zacistijkrai/static/css/main.b2a1e047.chunk.css"
+  },
+  {
+    "revision": "0d2494b12d08a33ca4c0",
+    "url": "/zacistijkrai/static/css/1.ccf1df40.chunk.css"
+  },
+  {
+    "revision": "f73fe2ba9da01b6d2f152d37a5bd7fbc",
+    "url": "/zacistijkrai/index.html"
+  }
+];
