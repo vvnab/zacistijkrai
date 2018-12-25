4095da41d5cf81e3ffc917293a68d0457e2c0114
Виталий Набережный <naberezhniy@taxi21.ru>
2018-12-25 16:42:07 +0300

    --

--- src/pages/main/components/header/index.jsx
+++ src/pages/main/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
+import _ from "lodash";
 import styles from "./index.module.scss";
 import { declOfNum } from "../../../../utils";
 import logo from "./logo.png";
@@ -8,16 +9,25 @@ const Header = ({ count, limit }) => (
   <div className="container-fluid">
     <div className="row middle-xs between-xs">
       <div className="col-xs-6">
-        <img src={logo} className={styles.img} alt="logo"/>
+        <img src={logo} className={styles.img} alt="logo" />
       </div>
       <div className="col-xs-6">
         <div className={styles.text}>
-          С нами {count < limit ? "пока": "уже"} <span className={styles.count}>{count}</span>{" "}
-          {declOfNum(count, ["человек", "человека", "человек"])}
+          {count > 0 ? (
+            <span>
+              С нами {count < limit ? "пока" : "уже"}{" "}
+              <span className={styles.count}>{count}</span>{" "}
+              {declOfNum(count, ["человек", "человека", "человек"])}
+            </span>
+          ) : (
+            <span>Идёт загрузка...</span>
+          )}
         </div>
       </div>
     </div>
   </div>
 );
 
-export default connect(state => ({count: state.main.payload.docCount}))(Header)
+export default connect(state => ({ count: state.main.payload.docCount }))(
+  Header
+);
