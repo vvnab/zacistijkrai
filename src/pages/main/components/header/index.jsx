b4867949a3a5670dc9464f0215ee8559740fcd7c
Виталий Набережный <naberezhniy@taxi21.ru>
2019-01-16 14:38:27 +0300

    --

--- src/pages/main/components/header/index.jsx
+++ src/pages/main/components/header/index.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import styles from "./index.module.scss";
 import { declOfNum } from "../../../../utils";
-import logo from "./logo.png";
 
 const Header = ({ count, limit }) => (
   <div className="container-fluid">
     <div className="row middle-xs between-xs">
       <div className="col-xs-6">
         <div className={styles.logo}>
-          {/* <img src={logo} className={styles.img} alt="logo" /> */}
           <span className={styles.text}>
             Люди вместе, мусор - раздельно
           </span>
